dbb46b16908ec95cf8a50154ca0c2485a37dae29
bfcca-iscteiulpt <[bfcca@iscte-iul.pt]>
2018-05-08 13:34:22 +0100

    V0.9

---
+++ src/Java_to_MongoDB.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bson.Document;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+public class Java_to_MongoDB extends Thread {
+
+	private String message;
+	
+	public Java_to_MongoDB(String message){
+		this.message=message;
+	}
+	
+	//Transforma a mensagem num Document através de um hash
+	private static Document StringToDocument(String message){
+		Type type = new TypeToken<HashMap<String,String>>(){}.getType();
+		Map<String, Object> messageMap = new Gson().fromJson(message, type);
+		Document doc = new Document(messageMap);
+		return doc;
+	}
+	
+	@Override
+	public void run(){
+		
+		//Liga à base de dados MongoDB, e obtem a collection
+		@SuppressWarnings("resource")
+		MongoClient mongoClient = new MongoClient();
+		@SuppressWarnings("deprecation")
+		DB db = mongoClient.getDB("db_grupo11");
+		DBCollection collection = db.getCollection("humidadeTemperatura");
+		
+		//Cria o BasicDBObject que vai ser inserido na base de dados
+		BasicDBObject dbObject = new BasicDBObject(StringToDocument(message.toString()));
+						
+		collection.insert(dbObject);
+
+		System.out.println("Imported");
+		
+	}
+}
---
+++ src/Sensor_to_Java.java
@@ -0,0 +1,68 @@
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class Sensor_to_Java extends Thread{
+
+	
+	@Override
+	public void run(){
+		
+		MqttClient client;
+		MemoryPersistence persistence;
+		MqttConnectOptions conn;
+		
+		String broker = "tcp://iot.eclipse.org:1883";
+		String str = "password";
+		char[] accessKey = str.toCharArray();
+		String clientID = "userID";
+
+
+		try {
+			persistence = new MemoryPersistence();
+			client = new MqttClient(broker, clientID, persistence);
+			conn = new MqttConnectOptions();
+			conn.setCleanSession(true);
+			conn.setPassword(accessKey);
+			conn.setUserName(clientID);
+			client.connect(conn);
+			
+			if(client.isConnected()) {
+				System.out.println("Connected..");
+	    	}else {
+	    		System.out.println("Unable to connect");
+	    		System.exit(0);
+	    	}
+	    	           
+	    		
+	    	while(true){	
+			client.setCallback(new MqttCallback() {
+				public void connectionLost(Throwable cause) {}
+	    	        	
+				public void messageArrived(String topic, MqttMessage message) throws Exception {
+					if(message.toString().startsWith("{")){
+						System.out.println(message.toString());
+	    	
+						new Java_to_MongoDB(message.toString()).run();
+
+	    			}
+				}
+	    	        	
+				public void deliveryComplete(IMqttDeliveryToken token) {
+				}
+			});
+
+			client.subscribe("sid_lab_2018");
+	    	}        
+	    		
+		}catch(Exception x) {
+			x.printStackTrace();
+		}
+			
+			
+	}		
+			
+}
